deff912effbd19b1e4bf0ded94007bb0aa0cf34d
Hessein Mostafa <hesseinnmostafaa@gmail.com>
2024-10-10 14:42:05 +0300

    Delete src directory

--- src/app/component/payment/UserModule.ts
+++
@@ -1,9 +0,0 @@
-export class USERModul {
-    constructor(
-        public email: string,
-        public cardNumber: string,
-        public CVV: string,
-        public City: string,
-        public Address: string
-    ) { }
-}
