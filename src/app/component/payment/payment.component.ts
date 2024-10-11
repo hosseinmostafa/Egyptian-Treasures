90a5f6aadcb4d38203bfe3272820b0c1765cbd7a
omniaabuelhassan <omnya200489@gmail.com>
2024-10-11 23:25:27 +0300

    update the product style and make payment to dynamic add info

--- src/app/component/payment/payment.component.ts
+++ src/app/component/payment/payment.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { UserService } from '../../Services/user.service';
 import { USERModul } from './UserModule';
+import { Iproduct } from '../interfaces/Iproduct';
+import { CartService } from '../../Services/cart.service';
 
 @Component({
   selector: 'app-payment',
@@ -11,8 +13,20 @@ import { USERModul } from './UserModule';
 export class PaymentComponent {
   userModel: USERModul = new USERModul('', '', '', '', ''); // added type annotationhyy
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,private cartService:CartService) {}
+  products: Iproduct[] = [];
+ 
+ngOnInit(): void {
+  this.products = this.cartService.getCartItems();
+}
 
+getTotal(): number {
+  let total = 0;
+  this.products.forEach((product) => {
+    total += product.price;
+  });
+  return total;
+}
   // onSubmit() {
   //   this.userService.addUser(this.userModel).subscribe({
   //     next: (data) => console.log(data),
