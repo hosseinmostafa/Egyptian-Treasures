b6a32fcfea7699daa40ebbf72db417cb9aa7fa09
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-10-11 15:53:08 +0300

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,27 +8,19 @@ import { ProductDetailsComponent } from './component/product-details/product-det
 import { UsersComponent } from './component/users/users.component';
 import { ProductsComponent } from './component/products/products.component';
 import { NotFondComponent } from './component/not-fond/not-fond.component';
-<<<<<<< HEAD
 import { EditProfileComponent } from './component/edit-profile/edit-profile.component';
 
-=======
-import { AuthGuard } from './auth.guard'; 
->>>>>>> 6e8f350595ab6988a00bec4e88f6b83fd85e788c
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "sgin-in-sign-up", component: SginInSignUpComponent },
-  { path: "payment", component: PaymentComponent, canActivate: [AuthGuard] }, 
-  { path: "add-prouduct", component: AddProductComponent, canActivate: [AuthGuard] }, 
+  { path: "payment", component: PaymentComponent }, 
+  { path: "add-prouduct", component: AddProductComponent }, 
   { path: "product/:id", component: ProductDetailsComponent },
   { path: "users", component: UsersComponent },
   { path: "products", component: ProductsComponent },
-<<<<<<< HEAD
   { path: "edit-profile", component: EditProfileComponent},
   { path: "**", component: NotFondComponent },
-=======
-  { path: "", component: NotFondComponent },
->>>>>>> 6e8f350595ab6988a00bec4e88f6b83fd85e788c
 ];
 
 
