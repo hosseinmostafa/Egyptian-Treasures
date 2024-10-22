9954596dac3e82a85833876898a5c8edcd3d3f27
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-10-22 17:08:55 +0300

    style products

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -5,6 +5,7 @@ import { Iproduct } from '../interfaces/Iproduct';
 import { CartService } from '../../Services/cart.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ChangeDetectorRef } from '@angular/core';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-products',
@@ -33,8 +34,16 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.openSpinner1();
     this.loadProducts();
-  }
-
+    Aos.init({
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 900, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+    })
+    }
   ngOnChanges(): void {
     this.applyFilters();
     this.loadProducts();
